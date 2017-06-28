7e3d755a2e7c4a667477afa0f9e2b5f34edb9137
Ryan Apilado <ryan.apilado@gmail.com>
2017-06-27 20:23:32 -0400

    finish vector

---
+++ arrays-lists-and-tables/linked_list.h
@@ -0,0 +1,13 @@
+typedef struct ll_node {
+    int value;
+    struct ll_node *next;
+} ll_node;
+
+typedef linked_list {
+    ll_node *head;
+} linked_list;
+
+// add
+// delete n-th
+// search
+// init
---
+++ arrays-lists-and-tables/vector.h
@@ -0,0 +1,40 @@
+typedef struct {
+    int *array;
+    int n;
+    int capacity;
+} vector;
+
+void vector_add(vector *v, int value) {
+    if (v->n == v->capacity) {
+        int *new_array = malloc(sizeof(int) * v->capacity * 2);
+        int i;
+        for (i = 0; i < v->capacity; i++) {
+            new_array[i] = v->array[i];
+        }
+        free(v->array);
+        v->array = new_array;
+    }
+    v->array[v->n] = value;
+    v->n++;
+}
+
+void vector_delete(vector *v, int index) {
+    int i;
+    for (i = index; i < v->n - 1; i++) {
+        v->array[i] = v->array[i + 1];
+    }
+    v->n--;
+}
+
+void vector_print(vector *v) {
+    int i;
+    for (i = 0; i < v->n; i++) {
+        printf("%d ", v->array[i]);
+    }
+}
+
+void vector_init(vector *v, int capacity) {
+    v->n = 0;
+    v->capacity = capacity;
+    v->array = malloc(sizeof(int) * capacity);
+}
