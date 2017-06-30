c29d698a140ca049a65ced0e97ce3e4728b2f2b1
Ryan Apilado <ryan.apilado@gmail.com>
2017-06-30 00:08:33 -0400

    fix heap sort issue

--- heaps/binary_heap.h
+++ heaps/binary_heap.h
@@ -3,55 +3,46 @@
 
 typedef struct binary_heap {
     vector *v;
+    int (*f)(int a, int b);
 } binary_heap;
 
-int lesser(a, b) {
+enum order{
+    MIN,
+    MAX
+};
+
+int lesser(int a, int b) {
     return a < b;
 }
 
-int greater(a, b) {
+int greater(int a, int b) {
     return a > b;
 }
 
-void heapify_min(binary_heap *h, int i) {
-    heapify(h, &lesser);
-}
-
-void heapify_max(binary_heap *h, int i) {
-    heapify(h, &greater);
-}
-
-void heapify(binary_heap *h, int i, int *(f)(int)) {
+/* Heapify the i-th element in the heap. Not the element at index i!
+*/
+void heapify(binary_heap *h, int i) {
     vector *v = h->v;
     int max = i;
     int left = i * 2;
     int right = i * 2 + 1;
-    if (left <= v->n && f(v->array[left - 1], v->array[i - 1])) {
+    if (left <= v->n && h->f(v->array[left - 1], v->array[i - 1])) {
         max = left;
     }
-    if (right <= v->n && f(v->array[right - 1] < v->array[max - 1])) {
+    if (right <= v->n && h->f(v->array[right - 1], v->array[max - 1])) {
         max = right;
     }
     if (max != i) {
-        v->array[i - 1] = v->array[max - 1];
-        v->array[max - 1] = v->array[i - 1];
-        heapify(h, max, f);
+        v_swap(v, max - 1, i - 1);
+        heapify(h, max);
     }
 }
 
-void heap_build_min(binary_heap *h) {
-    heap_build(h, &lesser);
-}
-
-void heap_build_max(binary_heap *h) {
-    heap_build(h, &greater);
-}
-
-void heap_build(binary_heap *h, int *(f)(int)) {
+void heap_build(binary_heap *h)  {
     vector *v = h->v;
     int i;
     for (i = v->n / 2 - 1; i >= 0; i++) {
-        heapify(h, i, f);
+        heapify(h, i);
     }
 }
 
@@ -64,22 +55,39 @@ int heap_extract(binary_heap *h) {
     vector *v = h->v;
     int min = v->array[0];
     v->array[0] = v->array[v->n - 1];
-    min_heapify(h, 0);
+    heapify(h, 0);
     return min;
+}
+
+void bubble_up(binary_heap *h, int i) {
+    vector *v = h->v;
+    int parent = i / 2;
+    if (i == 1) {
+        return;
+    }
+    if (h->f(v->array[parent - 1], v->array[i - 1])) {
+        v_swap(v, i - 1, parent - 1);
+        bubble_up(h, parent);
+    }
 }
 
 void heap_insert(binary_heap *h, int k) {
     vector *v = h->v;
     v_add(v, k);
-    min_heapify(h, v->n - 1);
+    bubble_up(h, v->n);
 }
 
 void heap_print(binary_heap *h) {
     return;
 }
 
-binary_heap *heap_init(int capacity) {
+binary_heap *heap_init(int capacity, int order) {
     binary_heap *h = safe_malloc(sizeof(binary_heap));
     h->v = v_init(capacity);
+    if (order == MIN) {
+        h->f = &greater;
+    } else {
+        h->f = &lesser;
+    }
     return h;
 }
